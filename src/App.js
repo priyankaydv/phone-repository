a393c6b6e432cb8ee5db30d479ed456f2bf325fa
Priyanka Yadav <ydpriyanka01@gmail.com>
2021-07-03 17:03:16 +0530

    Styled 'Header' (inline styling)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Header from "./Header"
 
-function App() {
+class App extends Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Header/>
+    <button>Add</button>
+   <div>
+     <span>Name</span><br/>
+     <span>Phone</span>
+   </div>
+   </div>
   );
+}
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header =function(){
+    const HeaderStyle={textAlign: 'center',padding:20, background:'Black',color:'white',textTransform:'uppercase'}
+    return(
+        <div style={HeaderStyle}>
+      Phone directory
+    </div>
+    )
+}
+export default Header;
